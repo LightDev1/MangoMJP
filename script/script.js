b4a3ce3b92c4689ae7a4e0d3c4261f4de25fd6b4
LightDev1 <karumasanmy@gmail.com>
2020-06-20 21:53:55 +0400

    Day 3

--- script/script.js
+++ script/script.js
@@ -5,8 +5,9 @@ const menuBar = document.querySelector('.move__menu'),
     mainPage = document.querySelector('.main__page__content'),
     locationPath = document.querySelector('.location');
 
-const articlesMainContainer = document.createElement('div');
-const aboutContainer = document.createElement('div');
+const articlesMainContainer = document.createElement('div'),
+   aboutContainer = document.createElement('div'),
+   settingsContainer = document.createElement('div');
 
 const toggleBar = () => {
     menuBar.classList.toggle('move__menu__active');
@@ -107,6 +108,70 @@ const showAboutPage = () => {
   }
 };
 
+const showSettingsPage = () => {
+  if (!settingsContainer.classList.contains('active')) {
+    mainPage.classList.add('hide');
+
+    articlesMainContainer.classList.add('hide');
+    articlesMainContainer.classList.remove('active');
+    articlesMainContainer.innerHTML = '';
+
+    aboutContainer.classList.remove('active');
+    aboutContainer.style.display = 'none';
+    aboutContainer.innerHTML = '';
+
+    locationPath.textContent = 'Настройки';
+
+    toggleBar();
+
+    settingsContainer.className = 'settings__container';
+
+    const settings = document.createElement('div');
+    settings.className = 'settings';
+    settings.insertAdjacentHTML('beforeend', `
+        <div class="theme__settings">
+          <h2>Цветовая тема</h2>
+          <div class="dark__container">
+            <h3>Тёмная тема</h3>
+            <label class="switch">
+              <input type="checkbox">
+              <span class="slider round"></span>
+            </label>
+          </div>
+          <div class="colorized__container">
+            <h3>Выберите цвет темы</h3>
+            <div class="colors">
+                <button data-color="#880015" class="color__1"></button>
+                <button data-color="#ED1C24" class="color__2"></button>
+                <button data-color="#FF7F27" class="color__3"></button>
+                <button data-color="#FFF200" class="color__4"></button>
+                <button data-color="#22B14C" class="color__5"></button>
+                <button data-color="#00A2E8" class="color__6"></button>
+                <button data-color="#3F48CC" class="color__7"></button>
+                <button data-color="#A349A4" class="color__8"></button>
+                <button data-color="#B97A57" class="color__9"></button>
+                <button data-color="#FFAEC9" class="color__10"></button>
+                <button data-color="#FFC90E" class="color__11"></button>
+                <button data-color="#EFE4B0" class="color__12"></button>
+                <button data-color="#99D9EA" class="color__13"></button>
+            </div>
+          </div>
+        </div>
+        <div class="language__settings">
+            <h2>Выберите язык</h2>
+            <ul>
+                <li class="ru">Русский</li>
+                <li class="en">English</li>
+            </ul>
+        </div>
+    `);
+
+    tagMane.insertAdjacentElement('beforeend', settingsContainer);
+
+    settingsContainer.insertAdjacentElement('beforeend', settings);
+  }
+};
+
 const clickManger = (event) => {
     let target = event.target;
 
@@ -119,8 +184,12 @@ const clickManger = (event) => {
     }
 
     if (target.closest('.about')) {
-      showAboutPage();
-  }
+        showAboutPage();
+    }
+
+    if (target.closest('.settings')) {
+        showSettingsPage();
+    }
 };
 document.addEventListener('swipe', () => {
     menuBar.classList.add('move__menu__active');
